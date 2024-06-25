1462994ea1c611259ec489efb0a20b025ffff1b7
Dileepa Praveen <118685287+Praveen1214@users.noreply.github.com>
2024-06-26 00:40:17 +0530

    RPC | add front end validation

--- src/components/PriceDisplay.jsx
+++ src/components/PriceDisplay.jsx
@@ -10,33 +10,34 @@ const PriceDisplay = ({ adultCost = 0, childrenCost = 0, totalPrice = 0 }) => {
     };
 
     return (
-        <div className="w-full border-b">
+        <div className="w-full ">
             <h3 className="text-lg font-bold mb-2">Cost</h3>
             <table className="w-full">
                 <tbody>
                     <tr className="border-b">
-                        <td className="py-2 font-small">Items</td>
-                        <td className="py-2 font-small text-right">Amount</td>
+                        <td className="py-3 px-2 font-small">Items</td>
+                        <td className="py-2 px-3 font-small text-right">Amount</td>
                     </tr>
 
                     <tr>
-                        <td className="py-2">Adult Cost</td>
-                        <td className="py-2 text-right">{formatCurrency(adultCost)}</td>
+                        <td className="py-3 px-2 ">Adult Cost</td>
+                        <td className="py-2 px-3 text-right">{formatCurrency(adultCost)}</td>
                     </tr>
                     <tr>
-                        <td className="py-2">Children Cost</td>
-                        <td className="py-2 text-right">{formatCurrency(childrenCost)}</td>
+                        <td className="py-3 px-2">Children Cost</td>
+                        <td className="py-2 px-3 text-right">{formatCurrency(childrenCost)}</td>
                     </tr>
                     <tr
-                        className="font-bold mt-12"
+                        className="font-bold "
                         style={{
-                            padding: "8px",
+                            
+                            padding: "20px",
                             borderRadius: "8px",
                             border: "0.784px solid var(--Color-Usage-Stroke, #E0E0E0)"
                         }}
                     >
-                        <td className="py-2">Total Price</td>
-                        <td className="py-2 text-right">{formatCurrency(totalPrice)}</td>
+                        <td className="py-3 px-2">Total Price</td>
+                        <td className="py-2 px-3 text-right">{formatCurrency(totalPrice)}</td>
                     </tr>
                 </tbody>
             </table>
